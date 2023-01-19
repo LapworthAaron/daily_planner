3b8f01d2f3842895e1ee5bb7174dfde7a319d5a8
Aaron Lapworth <aaronlapworth@hotmail.co.uk>
2023-01-19 13:02:33 +0000

    current day, timeslot time, color agenda item

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,15 +12,24 @@ function initTimeslots() {
         timeSlot.attr('class', 'time-block hour col-1');
         timeSlot.css('border-radius','0');
         timeSlot.appendTo($(row));
-        timeSlot.text(i);
+        timeSlot.text(moment().hour(i).minute(0).second(0).format("HA"));
     
         //setup agenda blocks
         var agenda = $('<textarea>');
-        agenda.attr('class', 'future col-9');
         agenda.css({'border-radius':'0','text-align':'left','padding-top':'10px'});
         agenda.attr('contenteditable','true');
         agenda.appendTo($(row));
-        
+        //set agenda colour based on time of day
+        if (moment().hour(i).minute(0).second(0).format("HA") ===
+            moment().minute(0).second(0).format("HA")) {
+                agenda.attr('class', 'present col-9');
+        } else if (moment().hour(i).minute(0).second(0) <
+                    moment().minute(0).second(0)) {
+                        agenda.attr('class', 'past col-9'); 
+                } else {
+                    agenda.attr('class', 'future col-9'); 
+                }
+
         //setup buttons with icon
         var button = $('<button>');
         button.attr('class', 'time-block saveBtn col-2');
@@ -36,14 +45,20 @@ initTimeslots();
 
 
 //function to display current day
+//TODO: moment.js stuff
 function currentDay() {
-    $('#currentDay') = momentjsStuff;
+    $('#currentDay').text(moment().format("dddd Do MMMM YYYY H"));
 }
+currentDay();
 
 
-//function to pick up on click of agenda items
+//function for setting classes of agenda items based on time
+//TODO: 
 function agendaClick() {
-
+    $('#currentDay').text(moment().format("H"));
 }
+
+
+
 
 // $('.time-block').on('click', agendaClick);
