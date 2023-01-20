3fff1b6131a31b54b6c2dacd6a03711642b737b5
Aaron Lapworth <aaronlapworth@hotmail.co.uk>
2023-01-20 19:01:48 +0000

    tidy code and comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//declare array used for localStorage
 var agendaArray = 
     {"9AM": "",
     "10AM": "",
@@ -9,35 +10,40 @@ var agendaArray =
     "4PM": "",
     "5PM": ""}
 
+//initialise page with contents
 init();
+$('.time-block').on('click', saveAgenda);
 
 //setup timeslots
 function init() {
     currentDay();
 
+    //create a new div for bootstrap rows
     var row = $('<div>');
     row.attr('class', 'row');
     row.appendTo($('.container'));
-    row.css({'text-align': 'center',
-            'margin': '50px'});
+    row.css({'margin-bottom': '50px'});
     
-    //**this needs to use moment.js set time for timeSlot **/
+    //loop though times and create the timebox div, agenda textarea and save button
     for (var i = 9; i < 18; i++) {
-        //setup time boxes
+        //setup time boxes and their attributes
         var timeSlot = $('<div>');
-        timeSlot.attr('class', 'time-block hour col-1');
-        timeSlot.attr('id', 'time_' + i);
+        timeSlot.attr({'class': 'time-block hour col-1',
+                        'id': 'time_' + i });
         timeSlot.css({'border-radius': '0',
                       'text-align': 'left',
                       'padding': '40px 0 0 5px'});
         timeSlot.appendTo($(row));
+        //display i as a time in the timeSlot div i.e 16 -> 4PM 
         timeSlot.text(moment().hour(i).minute(0).second(0).format("hA"));
     
-        //setup agenda blocks
+        //setup agenda blocks and their attributes
         var agenda = $('<textarea>');
-        agenda.css({'border-radius':'0','text-align':'left','padding-top':'10px'});
-        agenda.attr('contenteditable','true');
-        agenda.attr('id', 'agenda_' + i);
+        agenda.css({'border-radius':'0',
+                    'text-align':'left',
+                    'padding-top':'10px'});
+        agenda.attr({'contenteditable':'true',
+                    'id': 'agenda_' + i});
         agenda.appendTo($(row));
         //set agenda colour based on time of day
         if (moment().hour(i).minute(0).second(0).format("HA") ===
@@ -50,7 +56,7 @@ function init() {
                     agenda.attr('class', 'future col-10'); 
                 }
 
-        //get values from local storage
+        //get agenda text values from local storage
         getAgendas();
 
         //setup buttons with icon
@@ -58,10 +64,12 @@ function init() {
         button.attr('class', 'time-block saveBtn col-1');
         button.appendTo($(row));
         button.css('border-radius','0 10px 10px 0');
+        //add icon of floppy disk in the button
         var icon = $('<i>');
         icon.attr('class', 'fa fa-save');
         icon.appendTo($(button));
     }
+    return;
 }
 
 //function to display current day
@@ -69,11 +77,11 @@ function currentDay() {
     $('#currentDay').text(moment().format("dddd Do MMMM YYYY"));
 }
 
-//funtion to get local storage values
+//funtion to get local storage values if they exist
 function getAgendas() {
     if (localStorage.getItem("agendaItems")) {
         agendaArray =  JSON.parse(localStorage.getItem("agendaItems"));
-        for (var i = 9; i < 19; i++) {
+        for (var i = 9; i < 18; i++) {
             var timeSlot = $('#time_' + i);
             hourName = moment().hour(i).minute(0).second(0).format("hA");
             timeSlot.next().val(agendaArray[hourName]);
@@ -88,28 +96,25 @@ function getAgendas() {
 function addAgendas(agendaTime, agendaValue) {
     if (getAgendas() != undefined) {
         agendaArray = getAgendas();
-        // tempAgenda.push({time: value});
         agendaArray[agendaTime] = agendaValue;
     } else {
-        // tempAgenda.push({time: value});
         agendaArray[agendaTime] = agendaValue;
     }
     localStorage.setItem("agendaItems", JSON.stringify(agendaArray));
 }
 
-//save item
+//save item to local storage
 function saveAgenda(event) {
-    // $(event.target).siblings('.col-9').empty();
+   //if they click the icon then go to the button parent and use prev to read the time and agenda
     if (event.target.nodeName == 'I') {
         var val = $(event.target).parent().prev().val();
         var time = $(event.target).parent().prev().prev().text();
         addAgendas(time, val);
     } else {
+        // if they click the button use prev to read the time and agenda
         var val = $(event.target).prev().val();
         var time = $(event.target).prev().prev().text();
         addAgendas(time, val);
     }
     getAgendas();
 }
-
-$('.time-block').on('click', saveAgenda);
